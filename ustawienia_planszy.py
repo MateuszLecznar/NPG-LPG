2d4ff22b27a121291aa4d0880f0d1f959e6d3bf6
Gabriel Maj <majgabriel@student.agh.edu.pl>
2021-06-04 19:55:28 +0200

    x

--- ustawienia_planszy.py
+++ ustawienia_planszy.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+#ustawienia planszy
+plansza = [[None] * 3, [None] * 3, [None] * 3]
+#okno programu
+
+pg.init()
+fps = 30
+zegar = pg.time.Clock()
+ekran = pg.display.set_mode((szerokosc, wysokosc + 300), 0, 32)
+pg.display.set_caption("Kolko i krzyzyk")
--- ustawienia_poczatkowe.py
+++ ustawienia_poczatkowe.py
@@ -1,2 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+
+import random
+import keyboard
+import pygame as pg, sys, time
+from pygame.locals import *
+czyj_ruch = "X"
+kto_wygral = None
+remis = False
+szerokosc = 400
+wysokosc = 400
+
+bialy = (255, 255, 255)
+czarny = (0, 0, 0)
+czerwony = (255, 0, 0)
+
+punkty_X = 0
+punkty_Y = 0
+powtorka =0
+czy_moge=0
